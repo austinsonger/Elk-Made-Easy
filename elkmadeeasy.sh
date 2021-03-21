9a6ce4b66a642ac62fbe024fb3bc31c9c29dda75
Austin Songer <austinvernsonger@protonmail.com>
2021-03-20 23:41:51 -0500

    Update elkmadeeasy.sh

--- elkmadeeasy.sh
+++ elkmadeeasy.sh
@@ -1,217 +1,166 @@
-#!/bin/bash
-#
-# OS: Debian-base Systems
-#
-###################################################
+#! /bin/bash
+# Email : austin@songer.pro
+# Website : https://songer.pro
+# ELK Made Easy
+# ------------------------------#
+# UBUNTU 18.04, 20.04
+# ------------------------------#
 
+clear
+echo -e "[>>] ----------------------------------------------------------- [<<]"
+echo -e "[>>] Website : https://songer.pro                                [<<]"
+echo -e "[>>] Github : https://github.com/austinsonger                    [<<]"
+echo -e "[>>] Email : austin@songer.pro                                   [<<]"
+echo -e "[>>] ----------------------------------------------------------- [<<]"
+echo -e "[>>] ELK Made Easy                                               [<<]"
+echo -e "[>>] UBUNTU 18.04, 20.04                                         [<<]"
+echo -e "[>>] ----------------------------------------------------------- [<<]"
 
-echo "--------------------------------------------------------------------------"
-echo "$(date)"
-echo "Starting ELK+ Made Easy"
-echo "ELK Stack for Debian-based Systems"
-echo "Elasticsearch - Logstash - Kibana - Metricbeat - Packetbeat - Auditbeat"
-echo "-------------------------------------------------------------------------"
-
-echo -e "ELK+ Made Easy Status" #White
-
-echo " System Update..."
-
-# Checking whether user has enough permission to run this script
 sudo -n true
-sudo apt-get update 
-sudo apt-get upgrade -y
-sudo apt-get install openjdk-8-jre-headless -y
-# add-apt-repository ppa:webupd8team/java
-echo ">> Pre-agreeing to Oracle License"
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-  sudo debconf-set-selections
-echo ">> Installing Java"
-# apt-get -y install oracle-java8-installer 
-sudo echo $JAVA_HOME
-sudo apt-get install curl apt-transport-https software-properties-common lsb-release gnupg2 dirmngr sudo expect net-tools -y
-curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
-
-######################################################################
-# Install ELK + Metricbeat, Packetbeat, and Auditbeat on Debian/Ubuntu
-######################################################################
-
-##########################################
-# Install Elasticsearch
-##########################################
-echo "---- Installing the Elasticsearch Debian Package ----"
-sudo wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.8.1-amd64.deb
-sudo dpkg -i elasticsearch-7.8.1-amd64.deb
-sudo rm elasticsearch*
-# apt-get install elasticsearch=7.5.0 -y --allow-downgrades
-sed -i "s/^#network\.host/network.host/" /etc/elasticsearch/elasticsearch.yml
-sed -i "s/^#http\.port/http.port/" /etc/elasticsearch/elasticsearch.yml
-sed -i 's/^#node\.name: node\-1/node\.name: node\-1/'i /etc/elasticsearch/elasticsearch.yml
-sed -i 's/^#cluster\.initial_master_nodes: \["node-1", "node-2"]/cluster.initial_master_nodes: ["node-1"]'/i /etc/elasticsearch/elasticsearch.yml
-#---------------------------------------
-echo "---- Starting Elasticsearch ----"
-systemctl daemon-reload
-systemctl enable elasticsearch
-systemctl start elasticsearch
-systemctl restart elasticsearch
-echo RESTARTING Elasticsearch.......
-sleep 120
-
-#####################
-# Install kibana
-#####################
-echo "---- Installing the Kibana Debian Package ----"
-sudo wget https://artifacts.elastic.co/downloads/kibana/kibana-7.8.1-amd64.deb
-sudo dpkg -i kibana-7.8.1-amd64.deb
-sudo rm kibana*
-# apt-get install kibana=7.8.1 -y --allow-downgrades
-cp /etc/kibana/kibana.yml /tmp/
-my_ip=\""$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')\""
-sed -i "s/^#server\.host: \"localhost\"/server\.host: $my_ip/" /etc/kibana/kibana.yml
-sed -i "s/^#server\.port/server.port/" /etc/elasticsearch/elasticsearch.yml
-my_ip="$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}'):9200"
-sed -i "s/^#elasticsearch\.hosts/elasticsearch.hosts/" /etc/kibana/kibana.yml
-sed -i "s/^#elasticsearch\.url/elasticsearch.url/" /etc/elasticsearch/elasticsearch.yml
-sed -i "s/localhost:9200/$my_ip/" /etc/kibana/kibana.yml
-#---------------------------------------
-echo "---- Starting Kibana ----"
-systemctl enable kibana
-systemctl start kibana
-systemctl restart kibana
-echo Restarting Kibana.......
+sudo apt update -y
+sudo apt list --upgradable -a
+sudo apt upgrade -y
+sudo dpkg --configure -a
+sudo apt install -f
+sudo apt update --fix-missing
+sudo apt --fix-broken install -y
+apt -y -f install
+apt clean
+apt install net-tools
+apt install -y lsb-release &> /dev/null
+CHECK lsb_release 
+which lsb_release &> /dev/null
+[ "$?" != "0" ] && echo -e "\e[91m[>] WE CAN NOT FIND lsb_release COMMAND\e[0m" && exit !
+lsb_release -cs | grep 'focal' &> /dev/null
+[ "$?" != "0" ] && echo -e "\e[91m[>] WE CAN NOT INSTALL NETDATA IN YOUR OS\e[0m"
+apt install wget apt-transport-https curl gpgv gpgsm gnupg-l10n gnupg dirmngr nginx ca-certificates nginx software-properties-common apache2-utils jq -y
+sudo add-apt-repository universe
 sleep 10
 
 
-#####################
-# Install Filebeat
-#####################
-echo "---- Installing Filebeat ----"
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.8.1-amd64.deb
-sudo dpkg -i filebeat-7.8.1-amd64.deb
-sudo rm filebeat*
-# apt-get install filebeat=7.5.0 -y --allow-downgrades
-cp /etc/filebeat/filebeat.yml /tmp/
-my_ip="$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}'):9200"
-sed -i "s/YOUR_ELASTIC_SERVER_IP:9200/$my_ip/" /etc/filebeat/filebeat.yml
-#---------------------------------------
-echo "---- Starting Filebeat ----"
-sudo systemctl daemon-reload
-sudo systemctl enable filebeat
-sudo systemctl start filebeat
-sudo systemctl restart filebeat
+## JAVA 
+# openjdk-11-jdk
+sudo apt -y install openjdk-11-jdk
+# sudo echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+# source /etc/environment
+# sudo apt install default-jre default-jdk -y
+# update-alternatives --config java
+# export JAVA_HOME=/usr/lib/jvm/default-java-
+# export PATH=${PATH}:${JAVA_HOME}/bin
+sleep 5
 
-##########################################
-# Install Logstash
-##########################################
-echo "---- Installing Logstash ----"
-sudo wget https://artifacts.elastic.co/downloads/logstash/logstash-7.8.1.deb
-sudo dpkg -i logstash-7.8.1.deb
-sudo rm logstash*
-# apt-get install logstash=7.8.1 -y --allow-downgrades
-#---------------------------------------
-echo "---- Starting Logstash ----"
-sudo systemctl enable logstash
-sudo systemctl start logstash
-sudo systemctl restart logstash
-
-#####################
-# Install Metricbeat
-#####################
-echo "---- Installing Metricbeat ----"
-wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.8.1-amd64.deb
-sudo dpkg -i metricbeat-7.8.1-amd64.deb
-sudo rm metricbeat*
-# apt-get install metricbeat=7.8.1 -y --allow-downgrades
-#---------------------------------------
-echo "---- Starting Metricbeat ----"
-sudo systemctl enable  metricbeat
-sudo systemctl start metricbeat
-sudo systemctl restart metricbeat
-
-#####################
-# Install Packetbeat
-#####################
-echo "---- Installing Packetbeat ----"
-sudo apt-get install libpcap0.8
-wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-7.8.1-amd64.deb
-sudo dpkg -i packetbeat-7.8.1-amd64.deb
-sudo rm packetbeat*
-# apt-get install packetbeat=7.8.1 -y --allow-downgrades
-#---------------------------------------
-echo "---- Starting Packetbeat ----"
-sudo systemctl enable packetbeat
-sudo systemctl start packetbeat
-sudo systemctl restart packetbeat
-
-#####################
-# Install Auditbeat
-#####################
-echo "---- Installing Auditbeat ----"
-wget https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.8.1-amd64.deb
-sudo dpkg -i auditbeat-7.8.1-amd64.deb
-sudo rm auditbeat*
-# apt-get auditbeat=7.8.1 -y --allow-downgrades
-#---------------------------------------
-echo "---- Starting Auditbeat ----"
-sudo systemctl enable auditbeat
-sudo systemctl start auditbeat
-sudo systemctl restart auditbeat
+## Elasticsearch ##
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt-get update
+apt-get install elasticsearch -y
+systemctl stop elasticsearch
+systemctl enable elasticsearch
+cp /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.$RANDOM.backup
+echo 'transport.host: localhost' >> /etc/elasticsearch/elasticsearch.yml
+echo 'transport.tcp.port: 9300' >> /etc/elasticsearch/elasticsearch.yml
+echo 'network.host: localhost' >> /etc/elasticsearch/elasticsearch.yml
+echo 'http.port: 9200' >> /etc/elasticsearch/elasticsearch.yml
+echo 'discovery.type: single-node' >> /etc/elasticsearch/elasticsearch.yml
+echo 'xpack.security.enabled: true' >> /etc/elasticsearch/elasticsearch.yml
+echo 'setup.ilm.overwrite: true' >> /etc/elasticsearch/elasticsearch.yml
+echo '-Xms512m' >> /etc/elasticsearch/jvm.options
+echo '-Xmx512m' >> /etc/elasticsearch/jvm.options
+systemctl daemon-reload
+systemctl start elasticsearch
+sleep 25
+systemctl restart elasticsearch
+echo RESTARTING Elasticsearch.......
+sleep 60
 
 
-###################
-# Prevent Updates
-###################
+## Kibana ##
+sudo apt install kibana -y
+systemctl stop kibana
+systemctl enable kibana
+cp /etc/kibana/kibana.yml /etc/kibana/kibana.yml.$RANDOM.backup
+echo -e "server.port: 5601" >> /etc/kibana/kibana.yml
+echo -e "server.host: $HOSTNAME" >> /etc/kibana/kibana.yml
+echo -e 'elasticsearch.hosts: ["http://localhost:9200"]' >> /etc/kibana/kibana.yml
+echo -e 'elasticsearch.username: "elastic"' >> /etc/kibana/kibana.yml
+echo -e 'elasticsearch.password: "ChangePassword1!"' >> /etc/kibana/kibana.yml
+systemctl daemon-reload
+systemctl start kibana
+sleep 10
+
+
+## Logstash ##
+# INSTALL LOGSTASH PACKAGE 
+sudo apt install logstash -y
+sleep 10
+systemctl daemon-reload
+systemctl stop logstash
+systemctl enable logstash
+systemctl start logstash
+sleep 10
+
+## Filebeat ##
+# INSTALL FILEBEAT PACKAGE 
+sudo apt install filebeat -y
+cp /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.$RANDOM.backup
+sed -i 's/^output.elasticsearch/$output.elasticsearch/g' /etc/filebeat/filebeat.yml
+sed -i 's/hosts: \["127.0.0.1:9200"\]/#hosts: \["127.0.0.1:9200"\]/g' /etc/filebeat/filebeat.yml
+echo -e "output.logstash:" >> /etc/filebeat/filebeat.yml
+echo -e "hosts: [\"127.0.0.1:5044\"]" >> /etc/filebeat/filebeat.yml
+filebeat modules enable system
+filebeat setup --index-management -E output.logstash.enabled=false -E 'output.elasticsearch.hosts=["127.0.0.1:9200"]'
+systemctl start filebeat
+systemctl daemon-reload
+sleep 10
+
+## Prevent Updates ##
 sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update
 
-######################################
-# Protect Kibana with a reverse proxy
-######################################
+# Remove old package lists #
+############################
+rm -rf /var/lib/apt/lists/*
 
-echo "---- Installing and Configuring Reverse Proxy ----"
-apt install nginx -y
-mkdir -p /etc/ssl/certs /etc/ssl/private
-cp <ssl_pem> /etc/ssl/certs/kibana-access.pem
-cp <ssl_key> /etc/ssl/private/kibana-access.key
-mkdir -p /etc/ssl/certs /etc/ssl/private
-openssl req -x509 -batch -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/kibana-access.key -out /etc/ssl/certs/kibana-access.pem
-cat > /etc/nginx/sites-available/default <<\EOF
-server {
-    listen 80;
-    listen [::]:80;
-    return 301 https://$host$request_uri;
-}
-server {
-    listen 443 default_server;
-    listen            [::]:443;
-    ssl on;
-    ssl_certificate /etc/ssl/certs/kibana-access.pem;
-    ssl_certificate_key /etc/ssl/private/kibana-access.key;
-    access_log            /var/log/nginx/nginx.access.log;
-    error_log            /var/log/nginx/nginx.error.log;
-    location / {
-        auth_basic "Restricted";
-        auth_basic_user_file /etc/nginx/conf.d/kibana.htpasswd;
-        proxy_pass http://localhost:5601/;
-    }
-}
+# Clean #
+#########
+sudo apt autoremove --purge -y
+sudo apt autoclean -y
+sudo rm -rf /home/$USER/.local/share/Trash/*
+sudo find /tmp/ -type f -mtime +1 -exec sudo rm {} \;
+sudo find /tmp/ -type f -atime +1 -exec sudo rm {} \;
+sudo apt remove -y
+sudo apt clean -y
+sudo apt clean all -y
+
+### Nginx
+apt install nginx
+echo "admin:$(openssl passwd -apr1)" | tee -a /etc/nginx/htpasswd.users
+# echo -e "You need to set a username and password to login."
+# read -p "Please enter a username : " user
+# htpasswd -c /etc/nginx/conf.d/kibana.htpasswd $user
+touch /etc/nginx/sites-available/kibana
+cat > /etc/nginx/sites-available/kibana <<\EOF
+ server {
+     listen 80;
+      server_name $HOSTNAME;
+     auth_basic "Restricted Access";
+     auth_basic_user_file /etc/nginx/htpasswd.users;
+     location / {
+         proxy_pass http://127.0.0.1:5601;
+         proxy_http_version 1.1;
+         proxy_set_header Upgrade \$http_upgrade;
+         proxy_set_header Connection 'upgrade';
+         proxy_set_header Host \$host;
+         proxy_cache_bypass \$http_upgrade;
+     }
+ }
 EOF
-cp /etc/nginx/sites-available/default /tmp/
-my_ip="$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}'):5601"
-sed -i "s/localhost:5601/$my_ip/" /etc/nginx/sites-available/default
 
-apt install apache2-utils -y
+ln -s /etc/nginx/sites-available/kibana /etc/nginx/sites-enabled/
+
+nginx -t
+systemctl start nginx
 systemctl restart nginx
-systemctl restart elasticsearch kibana
-clear
-echo -e "You need to set a username and password to login."
-read -p "Please enter a username : " user
-htpasswd -c /etc/nginx/conf.d/kibana.htpasswd $user
-my_ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
-echo "All done! You can login under https://$my_ip"
+
 read -p "Press [Enter] to exit."
-
-
-
